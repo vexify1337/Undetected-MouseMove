f1dc221ed6902aca890927f3b3aa99477301becb
Lowkey Ud <122513007+vexify1337@users.noreply.github.com>
2025-03-22 05:42:30 +0000

    Add files via upload

---
+++ mouse.h
@@ -0,0 +1,15 @@
+#pragma once
+
+
+
+
+
+
+
+
+// UE5 Made this undetected mouse move func :3
+
+namespace UE5_Mouse {
+	void Mouse_Move(float x, float y); // good for moving your mouse for aimbot
+	void Mouse_Click(); // good for triggerbot
+}
